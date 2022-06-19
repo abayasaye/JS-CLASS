7af7e9f40301459309d552e9fc03e11091f610cb
abayasaye <abay400@gmail.com>
2022-06-19 18:10:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+// class Student {
+//     constructor (firstName,cRoom,iD){
+//         this.firstName = firstName ;
+//         this.cRoom = cRoom;
+//         this.iD = iD;
+
+//     }
+// }
+// const theStudent = new Student ("Abayneh" , "c12" , "23232323")
+// const myStudent = new Student ("or" , "c14" , "4453453")
+// console.log(theStudent,myStudent);
+
+// class User {
+//     constructor (firstName ,lastName , age , email) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age; 
+//         this.email = email;
+//     }
+// }
+
+// const firstUser = new User("abay" , "asaye" , "26" , "abay_43@gmail.com");
+// const secondUser= new User("or" , "gedamo" , "23" , "or_333@gmail.com");
+// const thirdUser= new User("batel" , "haiylo" , "24" ,"batel_232@gmail.com");
+// let userArray = [firstUser,secondUser, thirdUser]
+//  userArray.forEach(item=>{
+//     for(let key in item){
+// document.body.innerHTML += `<h1>${key}:${item[key]}</h1>`
+//     }
+    
+//  })
+
+//  function saveDetiles() {
+//     const newUser = new User (
+//     first_input.value,
+//     last_input.value,
+//     age_input.value,
+//     email_input.value
+//     );
+
+//     let newTable = document.createElement("table")
+//     document.body.appendChild(newTable)
+//     let tHead = document.createElement("thead")
+//     document.body.appendChild(tHead)
+//     let tBody = document.createElement("tbody")
+//     document.body.appendChild(tBody)
+//     let tableR = document.createElement("tr")
+//     document.body.appendChild(tableR)
+// for (key in newUser){
+//     let tableH = document.createElement("th")
+//     tableH.innerText += key + " ";
+//     tHead.appendChild(tableH)
+//     tableR.innerHTML += `<td>${newUser[key]}</td>` + " ";
+// }
+
+//  }
+
+// class Dogs {
+//     constructor (dogName , type , dathBirth){
+//         this.dogName = dogName;
+//         this.type = type;
+//         this.dathBirth= dathBirth;
+//     }
+// }
+ 
+// const dogsFirst = new Dogs("moli" , "bordercoli" , "23,12,2012");
+// const dogssecond = new Dogs("sky" , "boodog" , "11,10,2016");
+// const dogsthird = new Dogs("nebso" , "pitbol" , "28,11,2015");
+// const dogsForth = new Dogs("shahaf" , "pincher" , "30,8,2013");
+// const dogsFive = new Dogs("don" , "labrdor" , "14,9,2014");
+// const dogsArray = [
+//     dogsFirst,
+//     dogssecond,
+//     dogsthird,
+//     dogsForth,
+//     dogsFive
+// ]
+// dogsArray.forEach(item => {
+//     console.log(item)
+   
+// })
+
+
+class Teacher {
+    constructor(firstName , lastName , cRoom , email){
+
+        this.firstName= firstName;
+        this.lastName= lastName;
+        this.cRoom= cRoom;
+        this.email= email;
+    }
+}
+let teacherN
+const teachArray = []
