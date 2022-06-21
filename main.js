e32e405979d4886331c996ee3ca643fc2abaceef
abayasaye <abay400@gmail.com>
2022-06-21 14:40:02 +0300

    JS-CLASS

--- main.js
+++ main.js
@@ -37,6 +37,7 @@
 //     age_input.value,
 //     email_input.value
 //     );
+//     console.log(newUser)
 
 //     let newTable = document.createElement("table")
 //     document.body.appendChild(newTable)
@@ -81,51 +82,95 @@
 // })
 
 
-class Teacher {
-    constructor(firstName , lastName , cRoom , email){
+// class Teacher {
+//     constructor(firstName , lastName , cRoom , email){
 
-        this.firstName= firstName;
-        this.lastName= lastName;
-        this.cRoom= cRoom;
-        this.email= email;
-    }
-}
+//         this.firstName= firstName;
+//         this.lastName= lastName;
+//         this.cRoom= cRoom;
+//         this.email= email;
+//     }
+// }
 
-function theFuncSave () {
-    let teacherOne = new Teacher(
-        first_input.value,
-        second_input.value,
-         therd_input.value, 
-         four_input.value
-        );
-    const teachArray = [
-    
-    ]
-    if (teachArray.length < 4) {
-        teachArray.push(teacherOne)
-        console.log(teachArray)
-    }
-    teachArray.forEach(item => {
+// let teachArray = [];
+// function theFuncSave () {
+//     let teacherOne = new Teacher(
+//         first_input.value,
+//         second_input.value,
+//          therd_input.value, 
+//          four_input.value
+//         );
+//     if (teachArray.length < 4) {
+//         teachArray.push(teacherOne)
+//    console.log(teachArray)
+
+
+
+//     teachArray.forEach(item => {
         
-        let newTable = document.createElement("table")
-            document.body.appendChild(newTable)
-            let tHead = document.createElement("thead")
-            document.body.appendChild(tHead)
-            let tBody = document.createElement("tbody")
-            document.body.appendChild(tBody)
-            let tableR = document.createElement("tr")
-            document.body.appendChild(tableR)
+//         let newTable = document.createElement("table")
+//             document.body.appendChild(newTable)
+//             let tHead = document.createElement("thead")
+//             document.body.appendChild(tHead)
+//             let tBody = document.createElement("tbody")
+//             document.body.appendChild(tBody)
+//             let tableR = document.createElement("tr")
+//             document.body.appendChild(tableR)
          
-        for (let key in item) {
-                let tableH = document.createElement("th")
-                    tableH.innerText += key + " ";
-                     tHead.appendChild(tableH)
-                    tableR.innerHTML += `<td>${item[key]}</td>` + " ";
+//         for (let key in item) {
+//                 let tableH = document.createElement("th")
+//                     tableH.innerText +=key+ " ";
+//                      tHead.appendChild(tableH)
+//                     tableR.innerHTML += `<td>${item[key]}</td>` + " ";
            
-        }
-    }); 
-                
-                
-  
+//         }
+//     }); 
+// }       
+
+// }
+
+class Course {
+    constructor (courseName , isTheacher , gradesList , stuList){
+        this.courseName = courseName;
+        this.isTheacher = isTheacher;
+        this.gradesList = gradesList;
+        this.stuList = stuList;
+}
 
 }
+let mygraduList ;
+let myStuArray;
+const courseArray = []
+const newCourse = new Course ("mali" ,
+            true ,
+            mygraduList = ["99", "100" , "98" , "95" ,"96"] ,
+            myStuArray = ["daniel" , "ori" , "matan" , "stav" ,"amit"]
+);
+
+for (const key in newCourse) {
+    document.body.innerHTML += `<h3>${key}:${newCourse[key]} </h3>`+ " ";
+    mygraduList.sort((a,b)=> a-b)
+    myStuArray.sort()
+    if(Array.isArray(newCourse[key])){
+        document.body.innerHTML +=`<ul>${key}</ul>`;
+        newCourse[key].forEach(item => {
+            document.body.innerHTML +=`<ul><li>${item}</li></ul>`;
+            
+        });
+        
+    }
+    
+}
+// document.body.innerHTML +=`<ul>${newCourse[key][key]}</ul>`;
+
+// mygraduList.forEach(item => {
+//     document.body.innerHTML+= `<ul><li>${item}</li></ul>`;
+// });
+// myStuArray.forEach(item => {
+//     document.body.innerHTML+= `<ul><li>${item}</li></ul>`;
+// });
+courseArray.push(newCourse)
+console.log(courseArray)
+
+// mygraduList = ["<ul><li>90</li><li>100</li><li>100</li><li>98</li><li>99</li><li>95</li></ul>"] ,
+// myStuArray = ["<ul><li>danies</li><li>moshe</li><li>avi</li><li>shay</li><li>omer</li><li>natanel</li></ul>"]
